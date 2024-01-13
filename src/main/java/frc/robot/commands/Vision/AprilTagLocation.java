6ad92c701b7eeb42fc05d1e4e1121f7f1fb07564
Emiliano Lane <117385299+MichaelStatt-S@users.noreply.github.com>
2024-01-13 13:33:45 -0600

    add vender deps and fix copy over

--- src/main/java/frc/robot/commands/Vision/AprilTagLocation.java
+++ src/main/java/frc/robot/commands/Vision/AprilTagLocation.java
@@ -3,12 +3,10 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands.Vision;
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.apriltag.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.apriltag.AprilTagVision3;;
 
-public class AprilTagLocation extends CommandBase {
+public class AprilTagLocation extends Command{
   private AprilTagVision3 vision;
  /** Creates a new AprilTagLocation. */
   public AprilTagLocation(AprilTagVision3 vision3) {
